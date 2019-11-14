7c452ad29aa5c97f3e9fa70af707629649a06fc8
Jvsr <jvisser@student.codam.nl>
2019-11-14 10:37:04 +0100

    Norminette

--- includes/eventstate.h
+++ includes/eventstate.h
@@ -6,7 +6,7 @@
 /*   By: jvisser <jvisser@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/07 20:40:08 by jvisser        #+#    #+#                */
-/*   Updated: 2019/11/13 17:56:16 by jvisser       ########   odam.nl         */
+/*   Updated: 2019/11/14 10:35:35 by jvisser       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,26 @@
 
 # include <SDL2/SDL_events.h>
 
-typedef struct s_game			t_game;
+typedef struct s_game	t_game;
 
-void							initload_eventstate
-									(t_game *game, SDL_Event event);
-void							splash_eventstate
-									(t_game *game, SDL_Event event);
-void							mainmenu_eventstate
-									(t_game *game, SDL_Event event);
-void							missions_eventstate
-									(t_game *game, SDL_Event event);
-void							options_eventstate
-									(t_game *game, SDL_Event event);
-void							options_confirmation_eventstate
-									(t_game *game, SDL_Event event);
-void							controls_eventstate
-									(t_game *game, SDL_Event event);
-void							set_controls_eventstate
-									(t_game *game, SDL_Event event);
+void					initload_eventstate
+							(t_game *game, SDL_Event event);
+void					splash_eventstate
+							(t_game *game, SDL_Event event);
+void					mainmenu_eventstate
+							(t_game *game, SDL_Event event);
+void					missions_eventstate
+							(t_game *game, SDL_Event event);
+void					options_eventstate
+							(t_game *game, SDL_Event event);
+void					options_confirmation_eventstate
+							(t_game *game, SDL_Event event);
+void					controls_eventstate
+							(t_game *game, SDL_Event event);
+void					set_controls_eventstate
+							(t_game *game, SDL_Event event);
 
-typedef enum					e_eventstate_code
+typedef enum			e_eventstate_code
 {
 	initload,
 	splash,
@@ -44,19 +44,19 @@ typedef enum					e_eventstate_code
 	options_confirmation,
 	controls,
 	set_controls,
-}								t_eventstate_code;
+}						t_eventstate_code;
 
-typedef union					u_types
+typedef union			u_types
 {
 	int		dgt;
 	int		*dgtptr;
 	void	*ptr;
-}								t_types;
+}						t_types;
 
-typedef struct					s_eventstate
+typedef struct			s_eventstate
 {
 	t_eventstate_code	eventstate;
 	t_types				*arguments;
-}								t_eventstate;
+}						t_eventstate;
 
 #endif
